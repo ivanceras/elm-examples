bbe3b1b54e1773ab448b51798d48a2326ab72166
Jovansonlee Cesar <ivanceras@gmail.com>
2016-09-15 12:01:39 +0800

    get both scroll left and top

--- elm-scroll-divs/Main.elm
+++ elm-scroll-divs/Main.elm
@@ -3,7 +3,7 @@ import Html exposing (..)
 import Html.App as App exposing (..)
 import Html.Events exposing (..)
 import Html.Attributes exposing (..)
-import Json.Decode as Json
+import Json.Decode as Json exposing ((:=))
 
 
 main =
@@ -21,11 +21,11 @@ view model =
                 ,("height", "500px")
                 ,("overflow", "auto")
                 ]
-          ,onScrollTop ScrolledTop
+          ,onScroll Scrolled
          ]
          [ div [class "widget"
                 ,style [("background-color", "#cf9")
-                        ,("width", "200px")
+                        ,("width", "1200px")
                         ,("height", "700px")
                         ]
               ]
@@ -37,7 +37,7 @@ view model =
 type Msg 
     = Increment 
     | Decrement
-    | ScrolledTop Int
+    | Scrolled Scroll
 
 
 update msg model =
@@ -48,15 +48,28 @@ update msg model =
     Decrement ->
       model - 1
 
-    ScrolledTop top ->
+    Scrolled scroll ->
       Debug.log ("scrolled "++(toString msg))
       model
 
 
-targetScrollTop : Json.Decoder Int
-targetScrollTop =
-  Json.at ["target", "scrollTop"] Json.int
+type alias Scroll =
+ {left: Int
+  ,top: Int
+ }
 
-onScrollTop : (Int -> msg) -> Attribute msg
-onScrollTop tagger =
-  on "scroll" (Json.map tagger targetScrollTop)
+scrollDecoder: Json.Decoder Scroll
+scrollDecoder =
+    Json.object2 Scroll 
+         ("scrollTop" := Json.int)
+         ("scrollLeft" := Json.int)
+
+
+
+targetScroll : Json.Decoder Scroll
+targetScroll =
+  Json.at ["target"] scrollDecoder 
+
+onScroll : (Scroll -> msg) -> Attribute msg
+onScroll tagger =
+  on "scroll" (Json.map tagger targetScroll)
