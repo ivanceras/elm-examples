f1fc77cb0ca2640f5a6b2e54032d9626fb8b98a2
Jovansonlee Cesar <ivanceras@gmail.com>
2016-09-21 20:41:23 +0800

    truths

--- elm-ai/Ai.elm
+++ elm-ai/Ai.elm
@@ -49,5 +49,14 @@ addTruth List <words> = add truths into the program automatically add workds tha
     the order of words in truths is sensitive
 
 addSynonyms <word> List <words>
+ 
+how to add more truths that is longer than 2 arguments
+
+truth a
+truth2 a b
+truth3 a b c
+truth4 a b c d
+truth5 a b c d
+truth6 a b c d e
     
 --}
